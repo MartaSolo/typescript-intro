59127c3adc700bb69c722d9ed229b3f8d0c5f712
MartaSolo <marciamarcia@o2.pl>
2022-06-05 21:32:40 +0200

    edited ts file

---
+++ sandbox.js
@@ -0,0 +1,19 @@
+// any type
+var age;
+age = 25;
+var age = 25;
+age = true; // true
+age = 3; //3
+age = { name: "marta" }; // {name: 'marta'}
+console.log(age);
+//
+var mixed = [];
+mixed.push(false);
+mixed.push("bob");
+mixed.push(1);
+console.log(mixed);
+// [false, 'bob', 1]
+var mixedObject;
+mixedObject = { name: 13, age: "unknown" };
+console.log(mixedObject);
+// {name: 13, age: 'unknown'}
