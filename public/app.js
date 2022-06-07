4fd3d131cde4a1ae7ed4f5dffba1ffb0721f543d
MartaSolo <marciamarcia@o2.pl>
2022-06-07 14:42:35 +0200

    basics of interfaces

--- public/app.js
+++ public/app.js
@@ -1,4 +1,27 @@
 import { Invoice } from "./classes/invoice.js";
+const me = {
+    name: "marta",
+    age: 38,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log("I spend", amount);
+        return amount;
+    },
+};
+console.log(me);
+// {name: 'marta', age: 38, speak: ƒ, spend: ƒ}
+const greetPerson = (person) => {
+    console.log("hello", person.name);
+};
+greetPerson(me); // hello marta
+// classes
+const invoice1 = new Invoice("Bob", "plumbing job", 300);
+const invoice2 = new Invoice("Kate", "cleaning job", 150);
+console.log(invoice1);
+console.log(invoice2);
+// DOM
 const form = document.querySelector(".new-item-form");
 // inputs
 const type = document.querySelector("#type");
@@ -10,8 +33,3 @@ form.addEventListener("submit", (e) => {
     console.log(type.value, tofrom.value, details.value, amount.value);
     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
-// classes
-const invoice1 = new Invoice("Bob", "plumbing job", 300);
-const invoice2 = new Invoice("Kate", "cleaning job", 150);
-console.log(invoice1);
-console.log(invoice2);
--- scr/app.ts
+++ scr/app.ts
@@ -1,5 +1,43 @@
 import { Invoice } from "./classes/invoice.js";
 
+// interface
+interface IsPerson {
+  name: string;
+  age: number;
+  speak(a: string): void;
+  spend(a: number): number;
+}
+
+const me: IsPerson = {
+  name: "marta",
+  age: 38,
+  speak(text: string): void {
+    console.log(text);
+  },
+  spend(amount: number): number {
+    console.log("I spend", amount);
+    return amount;
+  },
+};
+
+console.log(me);
+// {name: 'marta', age: 38, speak: ƒ, spend: ƒ}
+
+const greetPerson = (person: IsPerson) => {
+  console.log("hello", person.name);
+};
+
+greetPerson(me); // hello marta
+
+// classes
+
+const invoice1 = new Invoice("Bob", "plumbing job", 300);
+const invoice2 = new Invoice("Kate", "cleaning job", 150);
+console.log(invoice1);
+console.log(invoice2);
+
+// DOM
+
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
 // inputs
@@ -13,10 +51,3 @@ form.addEventListener("submit", (e: Event) => {
   console.log(type.value, tofrom.value, details.value, amount.value);
   console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
-
-// classes
-
-const invoice1 = new Invoice("Bob", "plumbing job", 300);
-const invoice2 = new Invoice("Kate", "cleaning job", 150);
-console.log(invoice1);
-console.log(invoice2);
